1577abeda16f7099a6b5ef4946635516c3b29a4f
Raffi Hotter <raphael.hotter@gmail.com>
2024-11-03 18:25:57 -0800

    improvements

--- app/components/MathBlock.tsx
+++ app/components/MathBlock.tsx
@@ -83,13 +83,29 @@ function MathBlockView(props: any) {
     }
   }, [])
 
+  useEffect(() => {
+    // Focus the math field when it's created
+    setTimeout(() => {
+      mathFieldRef.current?.focus()
+    }, 0)
+  }, [])
+
+  const handleClick = () => {
+    mathFieldRef.current?.focus()
+  }
+
   return (
     <NodeViewWrapper>
-      <div className="my-4 p-4 bg-gray-50 rounded-lg text-center">
+      <div 
+        className="my-4 p-4 bg-gray-50 rounded-lg text-center cursor-text" 
+        onClick={handleClick}
+      >
         <math-field
           ref={mathFieldRef}
           default-mode="math"
           virtual-keyboard-mode="manual"
+          menu-editor="none"
+          className="border-none shadow-none"
         ></math-field>
         {result && (
           <div className="mt-2 text-gray-600">
