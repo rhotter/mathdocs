db0d13492e802bf2f9b3a1d18586b2f9cc770dda
Raffi Hotter <raphael.hotter@gmail.com>
2024-11-03 18:03:45 -0800

    v0, very basic skeleton

---
+++ app/components/MathBlock.tsx
@@ -0,0 +1,104 @@
+'use client'
+
+import { Node } from '@tiptap/core'
+import { ReactNodeViewRenderer, NodeViewWrapper } from '@tiptap/react'
+import { ComputeEngine } from '@cortex-js/compute-engine'
+import { useEffect, useRef, useState } from 'react'
+import 'mathlive'
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'math-field': any
+    }
+  }
+}
+
+const MathBlock = Node.create({
+  name: 'mathBlock',
+  group: 'block',
+  atom: true,
+
+  addAttributes() {
+    return {
+      latex: {
+        default: '',
+      },
+      result: {
+        default: '',
+      },
+    }
+  },
+
+  parseHTML() {
+    return [
+      {
+        tag: 'div[data-type="math-block"]',
+      },
+    ]
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return ['div', { 'data-type': 'math-block', ...HTMLAttributes }]
+  },
+
+  addCommands() {
+    return {
+      insertMathBlock:
+        () =>
+        ({ commands }) => {
+          return commands.insertContent({
+            type: this.name,
+            attrs: { latex: '', result: '' },
+          })
+        },
+    }
+  },
+
+  addNodeView() {
+    return ReactNodeViewRenderer(MathBlockView)
+  },
+})
+
+function MathBlockView(props: any) {
+  const mathFieldRef = useRef(null)
+  const [result, setResult] = useState('')
+  const ce = new ComputeEngine()
+
+  const calculateResult = (latex: string) => {
+    try {
+      const expr = ce.parse(latex)
+      const evaluated = expr.evaluate()
+      setResult(evaluated.latex)
+    } catch (error) {
+      console.error('Calculation error:', error)
+    }
+  }
+
+  useEffect(() => {
+    if (mathFieldRef.current) {
+      mathFieldRef.current.addEventListener('input', (evt: any) => {
+        calculateResult(evt.target.value)
+      })
+    }
+  }, [])
+
+  return (
+    <NodeViewWrapper>
+      <div className="my-4 p-4 bg-gray-50 rounded-lg text-center">
+        <math-field
+          ref={mathFieldRef}
+          default-mode="math"
+          virtual-keyboard-mode="manual"
+        ></math-field>
+        {result && (
+          <div className="mt-2 text-gray-600">
+            = {result}
+          </div>
+        )}
+      </div>
+    </NodeViewWrapper>
+  )
+}
+
+export default MathBlock
