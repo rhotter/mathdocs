1377c2e14de7d1f1ccf80bb520968ff485128e85
Raffi Hotter <raphael.hotter@gmail.com>
2024-11-10 23:05:13 -0800

    preliminary file storage

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,30 @@ import { useCallback, useEffect } from 'react'
 import MathBlock from './components/MathBlock'
 import MathInline from './components/MathInline'
 import { MathProvider } from './contexts/MathContext'
+import * as Y from 'yjs'
+import Collaboration from '@tiptap/extension-collaboration'
+import { TiptapCollabProvider } from '@hocuspocus/provider'
+import { useSearchParams } from 'next/navigation'
 
 export default function Home() {
+  const searchParams = useSearchParams()
+  const mode = searchParams.get('mode') || 'edit'
+  const docName = searchParams.get('doc') || 'default-doc'
+  
+  const ydoc = new Y.Doc()
+
   const editor = useEditor({
     extensions: [
-      StarterKit,
+      StarterKit.configure({
+        history: false,
+      }),
       MathBlock,
       MathInline,
+      Collaboration.configure({
+        document: ydoc,
+      }),
     ],
-    content: '<h1>Welcome to Mathdocs</h1><p>Write markdown and press CMD+E for inline math, or CMD+SHIFT+E for block math.</p>',
+    editable: mode === 'edit',
     editorProps: {
       attributes: {
         class: 'prose prose-slate max-w-none',
@@ -23,7 +38,30 @@ export default function Home() {
     immediatelyRender: false,
   })
 
+  useEffect(() => {
+    const provider = new TiptapCollabProvider({
+      name: docName,
+      appId: process.env.NEXT_PUBLIC_TIPTAP_APP_ID!,
+      token: process.env.NEXT_PUBLIC_TIPTAP_TOKEN!,
+      document: ydoc,
+
+      onSynced() {
+        console.log('synced')
+        if (!ydoc.getMap('config').get('initialContentLoaded') && editor) {
+          ydoc.getMap('config').set('initialContentLoaded', true)
+          console.log('synced')
+
+          editor?.commands.setContent(
+            '<h1>Welcome to Mathdocs</h1><p>Write markdown and press CMD+E for inline math, or CMD+SHIFT+E for block math.</p>',
+          )
+        }
+      }
+    })
+  }, [editor])
+
   const handleKeyDown = useCallback((event: KeyboardEvent) => {
+    if (!editor?.isEditable) return
+    
     if (event.metaKey && !event.shiftKey && event.key === 'e') {
       event.preventDefault()
       editor?.commands.insertMathInline()
